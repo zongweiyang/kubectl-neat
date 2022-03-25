90532334cc1d065ae167517f78dd42a144e85245
杨宗伟 <yzwok@foxmail.com>
2022-03-25 21:42:40 +0800

    Create generate-yaml.sh

---
+++ generate-yaml.sh
@@ -0,0 +1,6 @@
+for n in $(kubectl get -o=name configmap,secret,ingress,service,deployment,statefulset)
+do
+    mkdir -p $(dirname $n)
+    #kubectl get -o yaml $n > $n.yaml | /usr/local/sbin/kubectl-neat > /dev/null 2>&1
+    /usr/local/sbin/kubectl-neat get -- $n -oyaml > $n.yaml
+done
