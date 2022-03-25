30c662300f6d2232e980aa11f7cd5287498f6964
杨宗伟 <yzwok@foxmail.com>
2022-03-25 22:05:52 +0800

    Update generate-yaml.sh

--- generate-yaml.sh
+++ generate-yaml.sh
@@ -3,4 +3,5 @@ do
     mkdir -p $(dirname $n)
     #kubectl get -o yaml $n > $n.yaml | /usr/local/sbin/kubectl-neat > /dev/null 2>&1
     /usr/local/sbin/kubectl-neat get -- $n -oyaml > $n.yaml
+    echo "generated $n.yaml file"
 done
